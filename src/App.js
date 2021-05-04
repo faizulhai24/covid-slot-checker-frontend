b34534569ef3d3f5f2680a62a1db0de4fe312c9c
Shobhit Mandloi <shobhitmandloi.j11@gmail.com>
2021-05-04 13:07:27 +0530

    no free slots

--- src/App.js
+++ src/App.js
@@ -303,6 +303,11 @@ const App = () => {
                         </div>
                         {slotSearchSuccess &&
                         <div>
+
+                          <div>
+                            <label>No Free slots available. Please register to get notified when a slot opens up</label>
+                          </div>
+                          
                           <div className='textfield'>
                             <Field
                                 name="first_name"
